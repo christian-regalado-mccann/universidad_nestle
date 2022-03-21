dff28fad941a96052f69e60b1e1edf0b41994a30
epulla <erickpulladsi@gmail.com>
2022-03-21 18:36:39 -0500

    feat: Singleton for DB connection

--- src/Database.php
+++ src/Database.php
@@ -5,29 +5,43 @@
 *
 */
 
-
-define("DB_HOST", "localhost");
-define("DB_DATABASE", "vendelin_nestle_universidad");
-define("DB_USER", "vendelin_nestle");
-define("DB_PASS", "gHk(8m&3ev31");
-define("DB_PORT", "3306");
+include_once("utils/env_decoder.php");
+define("DB_HOST", getenv("DB_HOST"));
+define("DB_DATABASE", getenv("DB_DATABASE"));
+define("DB_USER", getenv("DB_USER"));
+define("DB_PASS", getenv("DB_PASS"));
+define("DB_PORT", getenv("DB_PORT"));
 
 
 class Database{
 
-    public function __construct(){}
+    private static $CONN_STR = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_DATABASE;
+    private static $instances = [];
 
-    public static function getDB(){
+    protected function __construct(){}
+
+    public static function getInstance() {
+        $subclass = static::class;
+        if (!isset(self::$instances[$subclass])) {
+            self::$instances[$subclass] = new static();
+        }
+        return self::$instances[$subclass];
+    }
+
+    protected function getConn() {
         try {
-            //code...
-            $con = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_DATABASE, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $con = new PDO(self::$CONN_STR, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $con;
         } catch (\Throwable $th) {
             return null;
-            //throw $th;
         }
+    }
+
+    public static function getDB(){
+        $database = self::getInstance();
+        return $database->getConn();      
     }
 
 }
